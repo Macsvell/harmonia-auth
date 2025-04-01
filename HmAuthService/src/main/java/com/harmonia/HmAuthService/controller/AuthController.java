9a6c17fc9ba36d93a13d4d4e03c6aa317935c44f
Macsvell <maxprogramr@mail.ru>
2025-04-01 15:14:52 +0300

    Rename class "AuthService" to "AuthController"

--- HmAuthService/src/main/java/com/harmonia/HmAuthService/controller/AuthController.java
+++ HmAuthService/src/main/java/com/harmonia/HmAuthService/controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/auth")
 @RestController
-public class AuthService {
+public class AuthController {
 
 	@GetMapping("/check/email")
 	public ResponseEntity<Object> checkEmail() {
